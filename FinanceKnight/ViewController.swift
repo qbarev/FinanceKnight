54da40b2958cbc4a484a311d33bcc31a04fccd94
Kirill Kubarev <kirill.kubareff@gmail.com>
2020-05-22 15:44:40 +0300

    Initial Commit

---
+++ FinanceKnight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinanceKnight
+//
+//  Created by Kirill Kubarev on 22.05.2020.
+//  Copyright © 2020 Kirill Kubarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
